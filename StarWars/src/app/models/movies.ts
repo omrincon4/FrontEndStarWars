0564053f449dd0737ba7cc468a51010470c9a891
Omar Geovani Rincon <omrincon4@poligran.edu.co>
2020-05-05 13:07:49 -0500

    Unificado de Vistas segun aportes

---
+++ StarWars/src/app/models/movies.ts
@@ -0,0 +1,11 @@
+export class Movie {
+
+    constructor(
+        public title: string,
+        public episode_id: string,
+        public opening_crawl: string,
+        public director: string,
+        public producer: string,
+        public release_date: string,
+    ){}
+}
---
+++ StarWars/src/app/models/specie.ts
@@ -0,0 +1,12 @@
+export class Specie {
+
+    // Modelo para razas
+    constructor(
+        public name: string,
+        public language: string,
+        public classification: string,
+        public average_height: string,
+        public designation: string,
+        public average_lifespan: string,
+    ){}
+}
